3e86daf554e61cf98907d4dc878ea485b8d72a6c
Joshua <jmgunter@users.noreply.github.com>
2023-12-02 23:06:02 -0600

    Updates

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,11 +1,19 @@
 import Header from "@/components/Header";
 import React from "react";
+import styles from "@/styles/Settings.module.css";
 
 export default function settings() {
   return (
     <div>
-      <Header />
-      {/* <input value={} type="number" /> */}
+      {/* <Header /> */}
+      <div className={styles.container}>
+        <div className={`${styles.gridItem} ${styles.header}`}>Header</div>
+        <div className={`${styles.gridItem} ${styles.sideBar}`}>
+          Sidebar <p>another</p>
+        </div>
+        <div className={styles.gridItem}>Content</div>
+        <div className={`${styles.gridItem} ${styles.footer}`}>Footer</div>
+      </div>
     </div>
   );
 }
